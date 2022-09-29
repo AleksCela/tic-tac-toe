ab1a22652dc21e9601ee5ebdfe714aeba68c2236
Aleks Cela <alekscela7@gmail.com>
2022-09-29 21:40:37 +0200

    random fixes

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
+const createPlayers = ( name, mark, turn) => {
+    return { name, mark, turn };
+};
+
+
 const gameBoard = (() => {
-    const createPlayers = ( name, mark, turn) => {
-        return { name, mark, turn };
-    };
-    const player1 = createPlayers('player 1', 'X', true);
-    const player2 = createPlayers('player 2', 'O', false);
 
-    let winner = null;
-    let turns = 0;
-
-    const board = ["", "", "", "", "", "", "", "", ""];
+    const board = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
 
     const resetBoard = () => {
         for (let i = 0; i < board.length; i++) {
           board[i] = "";
         }
     };
+
+    //display the cells
+    let boardcontainer = document.querySelector('.boardcontainer');
+    board.forEach((item, index) => {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        boardcontainer.appendChild(cell);
+        cell.innerText=item;
+    })
+
+
+    return {
+        board
+    };
     
-    
+})
+
+const game = (() => {
+
+    const player1 = createPlayers('player 1', 'X', true);
+    const player2 = createPlayers('player 2', 'O', false);
+
+    let winner = null;
+    let turns = 0;
+
+    let activeplayer = player1;
+
 })
